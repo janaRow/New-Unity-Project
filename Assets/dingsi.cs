bc3b192152310218ff8e140da42156f77b1fdb75
janaRow <jana.june1997@gmail.com>
2016-10-19 17:16:14 +0200

    hallo dings
    zeug example

---
+++ Assets/dingsi.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dingsi : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
